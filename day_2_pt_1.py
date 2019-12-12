4b8cf55e4ea28add71128a7c726f977ac3f71434
dsi <dsi@MacBook-Pro.local>
2019-12-11 23:28:59 -0800

    added comments

--- day_2_pt_1.py
+++ day_2_pt_1.py
@@ -1,21 +1,24 @@
 #test inputs
 #input = [1,9,10,3,2,3,11,0,99,30,40,50]
 #input = [1,1,1,4,99,5,6,0,99]
+#read in input file 
 f = open("day_2_input.txt", "r")
 nums = f.read()
 nums = list(nums.replace("\n", "").split(","))
 input_list = [int(n) for n in nums]
-#print(input_list)
+
+#make changes to list specified in problem 
 input_list[1] = 12
 input_list[2] = 2
-print(input_list[:3])
 
-
+#loop through list
 for i in range(len(input_list)):
+	#checking every 4 values to see if it is 99 (break)
 	if (i % 4 == 0) and (input_list[i] == 99):
 		print(input_list[0])
 		print("program broke")
 		break
+	#taking 1 and 0 and adding the following values in the list if 1 and mult if 2
 	elif (i % 4 == 0) and (input_list[i] != 99):
 		if input_list[i] == 1:
 			add = input_list[input_list[i + 1]] + input_list[input_list[i + 2]]
